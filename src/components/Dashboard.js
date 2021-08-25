39378abf8a0d73ec1f0098f773a6f054bfe2f4b9
Otabek Akbarov <Otabek.Akbaroff@gmail.com>
2021-08-25 17:28:09 -0500

    working on lists

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,9 +1,36 @@
-import React from 'react'
+import React,{useState,useEffect} from 'react'
+import axiosWithAuth from '../components/axiosWithAuth'
+
 
 function Dashboard(){
+    const [users, setUsers] = useState([])
+    useEffect(()=>{
+        axiosWithAuth().get('/api/users/all-users')
+        .then(response=>{
+              console.log(response)
+              setUsers(response.data)
+        })
+        .catch(err=>{
+              console.log(err);
+        })
+    },[])
+
+    const eventHandler = (e)=>{
+        console.log(e.id)
+        // props.history.push(`/dashboard${e.id}`);
+    }
+
     return(
         <div>
             <h1>Dashboard</h1>
+            <div>
+                {users.map(item=>(
+                    <div className="user" onClick={eventHandler} id = {item.id}>
+                        <h1>{item.username}</h1>   
+                        <h1>{item.id}</h1>
+                    </div>
+                ))}
+            </div>
         </div>
     )
 }
