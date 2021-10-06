f2d54bb2984177019adbf55b4d893bbae57b9d06
Otabek Akbarov <Otabek.Akbaroff@gmail.com>
2021-10-05 22:00:23 -0500

    added 404 to redirct to login page

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Switch,Route } from 'react-router-dom';
 import PrivateRoute from './components/PrivateRoute.jsx';
 import Dashboard from './components/dashboard/dashboard'
 import AuthPage from './components/authPage/authPage';
+import Login from './components/authPage/login/login.jsx';
 import './App.css'
 
 
@@ -13,6 +14,7 @@ function App() {
           <Route>
             <AuthPage/>
             <PrivateRoute path="/dashboard" component={Dashboard}/>
+            <Route  component={Login}/> {/* This should be error route (404) not found component */}
           </Route>
         </Switch>
       </Router>
