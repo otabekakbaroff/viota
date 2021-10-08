389202a337e73d8d8566ce4f16bcece314d554d4
Otabek Akbarov <Otabek.Akbaroff@gmail.com>
2021-10-08 15:52:32 -0500

    deployed version broken material ui trying old css

--- src/components/dashboard/users/children/search.jsx
+++ src/components/dashboard/users/children/search.jsx
@@ -6,22 +6,24 @@ import SearchIcon from '@material-ui/icons/Search';
 function Search(){
     const users_classes = usersStyles()
     return(
-        <div className={users_classes.search}>
+        <div className={users_classes.search} id="search" >
             <TextField 
-            className={users_classes.search_textField} 
+            className={users_classes.search_textField}
+            id="search_textField" 
             fullWidth 
             placeholder="Search of users..." 
             variant="outlined" 
             focused
             InputProps={{
                 className:users_classes.search_input,
+                id:"search_input",
                 endAdornment: (
                 <InputAdornment  >
                     <SearchIcon />
                 </InputAdornment>
                 )
             }} />
-            <SearchIcon className={users_classes.search_icon}/>
+            <SearchIcon className={users_classes.search_icon} id="search_icon" />
         </div>
     )
 }
