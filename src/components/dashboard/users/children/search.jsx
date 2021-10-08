78dcbe1a71859007ff3bf9d2c36f223c7f8302b6
Otabek Akbarov <Otabek.Akbaroff@gmail.com>
2021-10-08 16:17:56 -0500

    form control

--- src/components/dashboard/users/children/search.jsx
+++ src/components/dashboard/users/children/search.jsx
@@ -2,11 +2,12 @@ import usersStyles from "../usersStyles"
 import InputAdornment from "@material-ui/core/InputAdornment";
 import TextField from '@material-ui/core/TextField';
 import SearchIcon from '@material-ui/icons/Search';
+import FormControl from '@material-ui/core/FormControl'
 
 function Search(){
     const users_classes = usersStyles()
     return(
-        <div className={users_classes.search} id="search" >
+        <FormControl className={users_classes.search} id="search" >
             <TextField 
             className={users_classes.search_textField}
             id="search_textField" 
@@ -24,7 +25,7 @@ function Search(){
                 )
             }} />
             <SearchIcon className={users_classes.search_icon} id="search_icon" />
-        </div>
+        </FormControl>
     )
 }
 
