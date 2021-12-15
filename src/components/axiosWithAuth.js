5f5e5c1a9026f47bb0dfddf5c66da08bd93cc819
Otabek Akbarov <Otabek.Akbaroff@gmail.com>
2021-12-15 14:02:33 -0600

    deployed version

--- src/components/axiosWithAuth.js
+++ src/components/axiosWithAuth.js
@@ -2,8 +2,8 @@ import axios from 'axios';
 
 const axiosWithAuth=()=>{
     return axios.create({
-        // baseURL: 'https://viota-backend.herokuapp.com',
-        baseURL: 'http://localhost:5000',
+        baseURL: 'https://viota-backend.herokuapp.com',
+        // baseURL: 'http://localhost:5000',
         headers:{
             Authorization: localStorage.getItem('token')
         }
--- src/components/redux/action/index.js
+++ src/components/redux/action/index.js
@@ -4,8 +4,8 @@ import io from "socket.io-client"
 
 
 
-// export const socket = io('https://viota-backend.herokuapp.com',{autoConnect: true})
-export const socket = io('http://localhost:5000',{autoConnect: true})
+export const socket = io('https://viota-backend.herokuapp.com',{autoConnect: true})
+// export const socket = io('http://localhost:5000',{autoConnect: true})
 
 
 export const loginUser = usersInfo => dispatch => {
